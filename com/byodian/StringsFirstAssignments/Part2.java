e050d9d27abc0e7d55697d1cdd1d97e7bbd381d9
byodian <byj6696@gmail.com>
2023-03-21 21:18:44 +0800

    feat: optional class

--- com/byodian/StringsFirstAssignments/Part2.java
+++ com/byodian/StringsFirstAssignments/Part2.java
@@ -1,4 +1,5 @@
 package com.byodian.StringsFirstAssignments;
+import java.util.Optional;
 
 public class Part2 {
    public static String findSimpleGene(String dna, String startCodon, String stopCodon) {
@@ -8,6 +9,28 @@ public class Part2 {
 
     String formatedStopCodon = (stopCodon == null ? "TAA" : stopCodon).toLowerCase();
     int stopIndex = dna.toLowerCase().indexOf(formatedStopCodon, startIndex + 3);
+    if (stopIndex == -1)  { return ""; }
+
+    if ((stopIndex - startIndex) % 3 != 0) { return ""; }
+
+    String result = dna.substring(startIndex, stopIndex + 3);
+    return result;
+  }
+
+  public static String getOptionalParameter(String codon, String defaultCodon) {
+    Optional<String> optionalCodon = Optional.ofNullable(codon);
+    String optionalParameter = optionalCodon.isPresent() ? optionalCodon.get() : defaultCodon;
+    return optionalParameter;
+  }
+
+  public static String findSimpleGene2(String dna, String startCodon, String stopCodon) {
+    String lowerCaseDna = dna.toLowerCase();
+    String startCodonParameter = getOptionalParameter(startCodon, "ATG").toLowerCase();
+    int startIndex = lowerCaseDna.indexOf(startCodonParameter);
+    if (startIndex == -1) { return ""; }
+
+    String stopCodonParameter = getOptionalParameter(stopCodon, "TAA").toLowerCase();
+    int stopIndex = lowerCaseDna.indexOf(stopCodonParameter, startIndex + 3);
     if (stopIndex == -1)  { return ""; }
 
     if ((stopIndex - startIndex) % 3 != 0) { return ""; }
@@ -28,6 +51,12 @@ public class Part2 {
     System.out.println(findSimpleGene(dnaSequenceWithNonMultipleSubstr, null, null));
     System.out.println(findSimpleGene(dnaSequenceWithMultipleSubstr1, null, null));
     System.out.println(findSimpleGene(dnaSequenceWithMultipleSubstr2, null, null));
+
+    System.out.println(findSimpleGene2(geneWithoutStartCodon,null, null));
+    System.out.println(findSimpleGene2(geneWithoutStopCodon, null, null));
+    System.out.println(findSimpleGene2(dnaSequenceWithNonMultipleSubstr, null, null));
+    System.out.println(findSimpleGene2(dnaSequenceWithMultipleSubstr1, null, null));
+    System.out.println(findSimpleGene2(dnaSequenceWithMultipleSubstr2, null, null));
   }
 
   public static void main(String[] args) {
