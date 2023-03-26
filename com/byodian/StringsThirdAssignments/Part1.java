442c7540e50945b4640cfd8d652c0d70e8fca8e1
byodian <byj6696@gmail.com>
2023-03-26 20:34:41 +0800

    storing all genes

--- com/byodian/StringsThirdAssignments/Part1.java
+++ com/byodian/StringsThirdAssignments/Part1.java
@@ -5,11 +5,10 @@ import edu.duke.StorageResource;
 
 public class Part1 {
     public static void main(String[] args) {
-        StorageResource geneList = getAllGenes("TAATGTAATGTAGTGAATGTGA");
-        for(String gene : geneList.data()) {
-            System.out.println(gene);
-        }
+        // testGetAllGenes();
+        // testCGRatioAndCountCTG();
 
+        testProcessGenes();
     }
 
     public static StorageResource getAllGenes(String dna) {
@@ -28,5 +27,77 @@ public class Part1 {
         }
 
         return geneList;
+    }
+
+    public static void testGetAllGenes() {
+        StorageResource geneList = getAllGenes("TAATGTAATGTAGTGAATGTGA");
+        for(String gene : geneList.data()) {
+            System.out.println(gene);
+        }
+    }
+
+    public static float cgRatio(String dna) {
+        int cgSum = 0 ;
+        for(int i = 0; i < dna.length(); i++) {
+            char c = dna.charAt(i);
+            if (c == 'C' || c == 'G') {
+                cgSum = cgSum + 1;
+            }
+        }
+
+        return (float) cgSum / dna.length();
+    }
+
+    public static int countCTG(String dna) {
+        int currentIndex = dna.indexOf("CTG");
+        int count = 0;
+        while (currentIndex != -1) {
+            count = count + 1;
+            currentIndex = dna.indexOf("CTG", currentIndex + 3);
+        }
+
+        return count;
+    }
+
+    public static void testCGRatioAndCountCTG() {
+        System.out.println(cgRatio("ATGCCATAG")); // 4/9 0.44444...
+        //                                ^     ^     ^  ^    ^
+        System.out.println(countCTG("CTGATACTGATACTGCTGATCTG"));
+    }
+
+    public static void processGenes(StorageResource sr) {
+        // the number of Strings in sr are longer than 9 characters
+        int sum1 = 0;
+        // the number of strings in sr whose C-G-ratio is higher than 0.35
+        int sum2 = 0;
+        int maxLengthGene = 0;
+        for(String s : sr.data()) {
+            if (s.length() > 9) {
+               System.out.println(s);
+               sum1 = sum1 + 1;
+            }
+
+            if (cgRatio(s) > 0.35) {
+                System.out.println(s);
+                sum2 = sum2 +1;
+            }
+
+           maxLengthGene = Math.max(maxLengthGene, s.length());
+        }
+
+        System.out.println("the number of Strings in sr are longer than 9 characters: " + sum1);
+        System.out.println("the number of strings in sr whose C-G-ratio is higher than 0.35: " + sum2);
+        System.out.println("the length of the longest gene in sr: " + maxLengthGene);
+    }
+
+    public static void testProcessGenes() {
+        String str1 = "ATAATGT";
+        String str2 = "ATGTTTAATTAAATGATTTAA";
+        String str3 = "ATGCCCGGGTAAATGCGCATGTGA";
+        String str4 = "ATGATTAGATAA";
+        processGenes(getAllGenes(str1));
+        processGenes(getAllGenes(str2));
+        processGenes(getAllGenes(str3));
+        processGenes(getAllGenes(str4));
     }
 }
