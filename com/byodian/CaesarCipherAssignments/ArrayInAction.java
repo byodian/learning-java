bcc217c98f4f8fe8bb5ad6cb98772a5930df19e3
byodian <byj6696@gmail.com>
2023-03-30 22:09:26 +0800

    simpleSimulate with arrays

--- com/byodian/CaesarCipherAssignments/ArrayInAction.java
+++ com/byodian/CaesarCipherAssignments/ArrayInAction.java
@@ -1,11 +1,17 @@
 package com.byodian.CaesarCipherAssignments;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class ArrayInAction {
     public static void main(String[] args) {
-        textFingerPrint("Hello World!");
-        simpleSimulate(10);
+        // textFingerPrint("Hello World!");
+        // simpleSimulate(10);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+        scan.close();
+        simpleSimulateWithArrays(num);
     }
 
     public static void textFingerPrint(String s) {
@@ -15,7 +21,7 @@ public class ArrayInAction {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             int index = alpha.indexOf(Character.toLowerCase(ch));
-            System.out.println("index: " + index );
+
             if (index != -1) {
                 counters[index] += 1;
             }
@@ -26,6 +32,7 @@ public class ArrayInAction {
         }
     }
 
+    // long version
     public static void simpleSimulate(int rolls) {
         Random rand = new Random();
         int two = 0;
@@ -43,5 +50,20 @@ public class ArrayInAction {
 
         System.out.println("2's=\t" + two + "\t" + 100.0 * two/rolls);
         System.out.println("12's=\t" + twelves + "\t" + 100.0 * twelves/rolls);
+    }
+
+    public static void simpleSimulateWithArrays(int rolls) {
+        Random rand = new Random();
+        int[] results = new int[13];
+
+        for (int k = 0; k < rolls; k++) {
+            int d1 = rand.nextInt(6) + 1;
+            int d2 = rand.nextInt(6) + 1;
+            results[d1 + d2] += 1;
+        }
+
+        for (int i = 0; i < results.length; i++) {
+            System.out.println(i + "'s=\t" + results[i] + "\t" + 100.0 * results[i]/rolls);
+        }
     }
 }
