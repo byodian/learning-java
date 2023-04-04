bcf65cf66db9cf4fbb92418c8cbf3b9273cc7b5b
byodian <byj6696@gmail.com>
2023-04-04 16:57:29 +0800

    Finding Unique IP Adress

--- com/byodian/WebServerLogAssignments/LogAnalyzer.java
+++ com/byodian/WebServerLogAssignments/LogAnalyzer.java
@@ -24,4 +24,56 @@ public class LogAnalyzer {
             System.out.println(le);
         }
     }
+
+    public int countUniqueIps() {
+        ArrayList<String> iPList = new ArrayList<>();
+        for (LogEntry record : records) {
+            String ip = record.getIpAddress();
+
+            if (!iPList.contains(ip)) {
+                iPList.add(ip);
+            }
+        }
+
+        return iPList.size();
+    }
+
+    public void printAllHigherThanNum(int num) {
+        for (LogEntry record : records) {
+            int statusCode = record.getStatusCode();
+            if (statusCode > num) {
+                System.out.println(record);
+            }
+        }
+    }
+
+    public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+        ArrayList<String> uniqueIPList = new ArrayList<>();
+
+        for (LogEntry record : records) {
+           String currentDate = record.getAccessTime().toString();
+           String ip = record.getIpAddress();
+
+           if (currentDate.contains(someday) &&
+                   !uniqueIPList.contains(ip)
+           ) {
+               uniqueIPList.add(ip);
+           }
+        }
+
+        return uniqueIPList;
+    }
+
+    public int countUniqueIPsInRange(int low, int high) {
+        ArrayList<String> uniqueIPs = new ArrayList<>();
+        for(LogEntry record : records) {
+            String ip = record.getIpAddress();
+            int statusCode = record.getStatusCode();
+            if (!uniqueIPs.contains(ip) && statusCode >= low && statusCode <= high) {
+                uniqueIPs.add(ip);
+            }
+        }
+
+        return uniqueIPs.size();
+    }
 }
--- com/byodian/WebServerLogAssignments/Tester.java
+++ com/byodian/WebServerLogAssignments/Tester.java
@@ -1,13 +1,44 @@
 package com.byodian.WebServerLogAssignments;
+import java.util.ArrayList;
 
 public class Tester {
     public static void main(String[] args) {
-        testLogAnalyzer();
+        // testLogAnalyzer();
+        // testUniqueIP();
+        // testPrintAllHigherThanNum();
+        // testUniqueIPVisitsOnDay();
+        testCountUniqueIpsInRange();
     }
 
     public static void testLogAnalyzer() {
         LogAnalyzer la = new LogAnalyzer();
         la.readFile("short-test_log");
         la.printAll();
+    }
+
+    public static void testUniqueIP() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        System.out.println(la.countUniqueIps());
+    }
+
+    public static void testPrintAllHigherThanNum() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        la.printAllHigherThanNum(300);
+    }
+
+    public static void testUniqueIPVisitsOnDay() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("weblog-short_log");
+        ArrayList<String> uniqueIPs = la.uniqueIPVisitsOnDay("Sep 14");
+        System.out.println(uniqueIPs.size());
+    }
+
+    public static void testCountUniqueIpsInRange() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        System.out.println(la.countUniqueIPsInRange(200, 299));
+        System.out.println(la.countUniqueIPsInRange(300, 399));
     }
 }
